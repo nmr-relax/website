f1fe9a888b6add71e09546b65785849ddf4ac73b
Edward d'Auvergne <edward@nmr-relax.com>
2014-05-14 18:28:42 +0000

    Created a Python script for automatically creating all meta refresh redirect HTML pages.
    This currently creates two HTML pages which refresh to their intended current page.  One of these,
    api/fdl.html, has been added to the repository.

---
+++ .setup_redirects.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+# This Python script is for setting up all of the meta refresh redirects required for the relax website.
+
+# The list of redirects to build.  The first element is the old page, the second is the new page.
+redirects = [
+        ['manual/d_Auvergne_protocol_GUI_mode_relaxation_interaction.html', 'manual/d_Auvergne_protocol_GUI_mode_relaxation_interactions.html'],
+        ['api/fdl.html', 'fdl.html'],
+]
+
+# Loop over all redirects, automatically creating the webpage.
+for old, new in redirects:
+    # Open the old file for writing.
+    file = open(old, 'w')
+
+    # Add the contents.
+    file.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n")
+    file.write("<HTML>\n")
+    file.write("<HEAD>\n")
+    file.write("    <meta http-equiv=\"refresh\" content=\"0; url=http://www.nmr-relax.com/%s\">\n" % new)
+    file.write("</HEAD>\n")
+    file.write("</HTML>\n")
